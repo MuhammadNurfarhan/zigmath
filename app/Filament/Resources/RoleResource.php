627eaa968abccf4c7e22d16b228ac0bf273c9299
Muhammad Nurfarhan <mnfarhanz29@gmail.com>
2026-09-07 01:44:22 +0700

    feat: Add RoleResource and associated pages for role management

---
+++ app/Filament/Resources/RoleResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoleResource\Pages;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Spatie\Permission\Models\Role;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-key';
+
+    protected static ?string $navigationGroup = 'Manajemen User';
+
+    protected static ?string $navigationLabel = 'Role & Hak Akses';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Informasi Role')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->label('Nama Role')
+                            ->required()
+                            ->unique(ignoreRecord: true),
+
+                        Forms\Components\Hidden::make('guard_name')
+                            ->default('web'),
+
+                        Forms\Components\Select::make('permissions')
+                            ->label('Hak Akses')
+                            ->relationship('permissions', 'name')
+                            ->multiple()
+                            ->preload()
+                            ->searchable(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Role')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('permissions.name')
+                    ->label('Hak Akses')
+                    ->badge()
+                    ->limit(3),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d M Y'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoles::route('/'),
+            'create' => Pages\CreateRole::route('/create'),
+            'edit' => Pages\EditRole::route('/{record}/edit'),
+        ];
+    }
+}
